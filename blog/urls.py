79a56d09bb3eea4313eda2376e2037ea9ea4e9b3
Dilli Khatiwoda <dilli@dilli.com>
2018-03-31 16:59:51 -0500

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from .import views
 '''Add url patterns for blog app'''
 urlpatterns = [
     url(r'^$', views.post_list, name = 'post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name = 'post_detail'),
 
 ]
